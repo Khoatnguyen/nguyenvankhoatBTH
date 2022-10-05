889f4a860675476d0d622814fb4ccbf9e8e24a35
khoatngguyen <nguyenvankkhoat2000@gmail.com>
2022-10-05 16:48:41 +0700

    nguyen van khoat

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NGUYENVANKHOATBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
